2c996912c1b0346a762a47014e8de18b2ee27062
Ushasri <ushasrisamala2803@gmail.com>
2023-11-15 11:51:53 +0000

    solution

---
+++ src/components/Capitals/index.js
@@ -0,0 +1,81 @@
+import {Component} from 'react'
+import './index.css'
+
+const countryAndCapitalsList = [
+  {
+    id: 'NEW_DELHI',
+    capitalDisplayText: 'New Delhi',
+    country: 'India',
+  },
+  {
+    id: 'LONDON',
+    capitalDisplayText: 'London',
+    country: 'United Kingdom',
+  },
+  {
+    id: 'PARIS',
+    capitalDisplayText: 'Paris',
+    country: 'France',
+  },
+  {
+    id: 'KATHMANDU',
+    capitalDisplayText: 'Kathmandu',
+    country: 'Nepal',
+  },
+  {
+    id: 'HELSINKI',
+    capitalDisplayText: 'Helsinki',
+    country: 'Finland',
+  },
+]
+
+class Capitals extends Component {
+  state = {activeCapitalId: countryAndCapitalsList[0].id}
+
+  onChangeCapital = e => {
+    this.setState({activeCapitalId: e.target.value})
+  }
+
+  getCountry = () => {
+    const {activeCapitalId} = this.state
+    const activeCountry = countryAndCapitalsList.find(
+      eachCapital => eachCapital.id === activeCapitalId,
+    )
+
+    return activeCountry.country
+  }
+
+  render() {
+    const {activeCapitalId} = this.state
+    const country = this.getCountry(activeCapitalId)
+
+    return (
+      <div className="app-container">
+        <div className="current-container">
+          <h1 className="header">Countries And Capitals</h1>
+          <div className="capitals-container">
+            <select
+              className="capital-select"
+              onChange={this.onChangeCapital}
+              value={activeCapitalId}
+            >
+              {countryAndCapitalsList.map(eachCapital => (
+                <option
+                  value={eachCapital.id}
+                  key={eachCapital.id}
+                  className="option"
+                >
+                  {eachCapital.capitalDisplayText}
+                </option>
+              ))}
+            </select>
+            <p className="para"> &nbsp; is capital of which country?</p>
+          </div>
+          <p className="country-name">{country}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Capitals
